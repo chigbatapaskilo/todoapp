bb58137b8496ceaca3b199969fcb6790b79cf605
Emeka Chigbata <149632981+chigbatapaskilo@users.noreply.github.com>
2024-08-01 02:15:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const router = require("./router/userRouter")
-const todorouter=require("./router/todorouter")
+const todorouter=require("./router/todoRouter")
 
 
 require("./config/db")
